e85d36485d4ac5d1c22bd3a2efc9fa9d8567a0cb
Mostly-carbon <al.j.1029.abdul@gmail.com>
2026-05-16 15:40:39 +0800

    Update Smart_lib code

--- src/adt/LibraryADT.java
+++ src/adt/LibraryADT.java
@@ -1,1 +1,11 @@
+import model.Book;
+public interface LibraryADT {
 
+    void addBook(String title, String author, String isbn);
+
+    Book searchBookByIsbn(String isbn);
+
+    boolean borrowBook(String isbn);
+
+    void viewBorrowingHistory();
+}
