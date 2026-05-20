1f38a10d62bb5263e09c4dc586b69150f0b44df0
Tawfique Hasan <25050842@siswa.um.edu.my>
2026-05-20 20:22:02 +0800

    Implement BookBST class with insert method

--- src/bst/BookBST.java
+++ src/bst/BookBST.java
@@ -1,1 +1,31 @@
+class BookBST {
+    private BSTNode root;
 
+    public BookBST() {
+        root = null;
+    }
+
+    // INSERT BOOK INTO BST
+    public void insert(Book book) {
+        root = insertRec(root, book);
+    }
+
+    private BSTNode insertRec(BSTNode root, Book book) {
+        if (root == null) {
+            return new BSTNode(book);
+        }
+
+        if (book.getIsbn().compareTo(root.book.getIsbn()) < 0) {
+            root.left = insertRec(root.left, book);
+        } else if (book.getIsbn().compareTo(root.book.getIsbn()) > 0) {
+            root.right = insertRec(root.right, book);
+        }
+
+        return root;
+    }
+
+    // Getter for root (used by teammate for recursive search)
+    public BSTNode getRoot() {
+        return root;
+    }
+}
