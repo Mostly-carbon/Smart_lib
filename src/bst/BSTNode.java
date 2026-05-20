b707083e8397ccb895c7e08accd57328e2ab6659
Tawfique Hasan <25050842@siswa.um.edu.my>
2026-05-20 20:21:10 +0800

    Add BSTNode class for binary search tree

--- src/bst/BSTNode.java
+++ src/bst/BSTNode.java
@@ -1,1 +1,11 @@
+class BSTNode {
+    Book book;
+    BSTNode left;
+    BSTNode right;
 
+    public BSTNode(Book book) {
+        this.book = book;
+        left = null;
+        right = null;
+    }
+}
